5066b03d1fe5f3deef6090d46c5e08b45539c92d
tundish <tundish@thuswise.org>
2013-09-04 08:06:03 +0100

    Added build_docs script

---
+++ tallywallet/common/doc/build_docs.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# Builds TopicMob web to go. Outputs the source package names.
+# eg:
+# $ ./topicmob/ops/scripts/build.sh --novenv --nopush --nosign > build.out
+
+PYTHON=/usr/local/bin/python3.4
+PYENV=~/py3.4
+SETUPTOOLS=setuptools-0.9.6
+PIP=pip-1.3.1
+
+DIR=$( cd "$( dirname "$0" )" && pwd )
+PARENT=$(readlink -e $DIR/../../../..)
+
+echoerr() { echo "$@" 1>&2; }
+
+cd $PARENT/tallywallet-common
+
+echoerr "Installing package to $PYENV ..."
+$PYENV/bin/python3 setup.py install > /dev/null
+
+echoerr "Building project documentation ..."
+$PYENV/bin/sphinx-build -c $PARENT/tallywallet-common/tallywallet/common/doc \
+    $PARENT/tallywallet-common/tallywallet/common/doc \
+    $PARENT/tallywallet-common/tallywallet/common/doc/_build
+
+
