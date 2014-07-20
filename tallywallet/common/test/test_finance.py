6c38ba2385a7f5e761f264013869610c47973da4
boss on Apple Mac <>
2014-07-20 18:51:54 +0100

    Placeholders for simple interest tests

--- tallywallet/common/test/test_finance.py
+++ tallywallet/common/test/test_finance.py
@@ -21,6 +21,7 @@ import unittest
 
 #prototyping
 from collections import namedtuple
+from decimal import Decimal
 
 Schedule = namedtuple("Schedule", ["n", "val"])
 
@@ -33,3 +34,18 @@ class ProgressionTests(unittest.TestCase):
         prog = list(itertools.takewhile(lambda i: i.n < 15, proc))
         self.assertEqual(41, prog[-1].val)
         self.assertEqual(300, sum(i.val for i in prog))
+
+
+class TestDiscountingAtSimpleInterest(unittest.TestCase):
+
+    def test_exact_simple_interest(self):
+        """MoF 2Ed 3.1"""
+        proc = (
+            Schedule(n, Decimal(0.145 * i / 365))
+            for n, i in enumerate(itertools.count(0, 1)))
+        prog = list(itertools.takewhile(lambda i: i.n < 60, proc))
+        self.fail(prog)
+
+    def test_ordinary_simple_interest(self):
+        """MoF 2Ed 3.1"""
+        self.fail()
